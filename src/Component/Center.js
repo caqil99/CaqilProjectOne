24cdc50d6cb7d2852f746425f64f88b0d39667d6
caaqil <caqil.bashir@gmail.com>
2023-04-14 05:31:57 -0900

    caqil project one

---
+++ src/Component/Center.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Center() {
+  return (
+    <div className='Center'>
+      <h1>MY NAME: Abdinasir Bashir Mohamed</h1>
+      <h1>MY ID C120238</h1>
+    </div>
+  );
+}
+
+export default Center;
